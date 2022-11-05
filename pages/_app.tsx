ea3cba95062f1abcaf7d5485258d31f2146db3ff
pandma <pablo.andreu.martin@gmail.com>
2022-11-05 13:01:34 +0100

    fixing css not rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,14 @@ import React, { Fragment } from "react";
 import Transition from "../components/Transition";
 import { useRouter } from "next/router";
 import "../styles/globals.css";
-
+import styles from '../styles/Home.module.css';
 
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <div style={{ backgroundColor: "black" }}>
+    <div className={styles.main}>
       <Fragment>
         <Transition location={router.pathname}>
           <Component {...pageProps} />
